447425383e69f7107e6e84871145d25e6aeba50f
Nika <129578493+nixiwqq@users.noreply.github.com>
2025-03-10 19:28:06 +0100

    Update and rename heyy.py to new-start.py

---
+++ new-start.py
@@ -0,0 +1,5 @@
+print("Nothing \nwill work \nunless you do")
+print()
+print()
+
+print(' \"Anyone who \n\tstops' '\n\t\tlearning is old, \n\t\t\twhether at twenty or eighty\" \n\t\t\t\t\t\tHenry Ford ')
